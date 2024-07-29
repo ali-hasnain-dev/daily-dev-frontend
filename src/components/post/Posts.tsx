7465a50f4ae4db7dcb044dda3c9958084f22deda
Ali Hasnain <alihasnainhfd@gmail.com>
2024-07-29 16:22:02 +0500

    broadcasting

--- src/components/post/Posts.tsx
+++ src/components/post/Posts.tsx
@@ -1,9 +1,20 @@
 "use client";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import PostCard from "./PostCard";
+import { laraEcho } from "@/lib/echo.config";
 
 export default function Posts({ data }: { data: APIResponseType<PostType> }) {
   const [posts, setPosts] = useState<APIResponseType<PostType>>(data?.posts);
+
+  useEffect(() => {
+    laraEcho.channel("test-channel").listen("TestEvent", (event: any) => {
+      console.log("the real time data is", event);
+    });
+
+    return () => {
+      laraEcho.leave("test-channel");
+    };
+  }, []);
 
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-4 pt-4 pl-2">
